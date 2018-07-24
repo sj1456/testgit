80fc11c79c69f0ef9d93949e515af1a2d580fe4d
Sai Jiang <tsjs2011@gmail.com>
2018-07-23 18:39:11 -0700

    First commit

---
+++ merge.py
@@ -0,0 +1,18 @@
+def merge(list1, list2):
+    # implement your solution here
+    
+    i=0
+    j=0
+    while i < len(list1):
+        if list2[j]>=list1[i]:
+            list2.insert(j,list1[i])
+            i+=1
+            j+=1
+        else:
+            j+=1
+      
+    return list2
+
+list1 = [1,3,8,19]
+list2 = [5,6,11,12,21]
+print str(merge(list2, list1))
